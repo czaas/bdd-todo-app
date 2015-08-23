7ec5b961734ceb45d66c3034d0f7a4fabb7faf70
Cameron Zaas <cameronzaas@gmail.com>
2015-08-23 12:28:19 -0700

    Initial commit

---
+++ tests/todo.js
@@ -0,0 +1,11 @@
+describe('My Todo App', function(){
+	
+	describe('Adding an item', function(){
+		it('Should collect a string and place it in array');
+	});
+	
+	describe('Removing an item', function(){
+		it('should remove specific string from array when deleted');
+	});
+	
+});
